96fb5e57c943b201d3674bc90d6e2becbf0c7b39
zhihao wang <hao@opentown.cn>
2015-10-10 18:08:02 +0800

    add some filter

--- transporter/transformers/passthrough_and_log.js
+++ transporter/transformers/passthrough_and_log.js
@@ -1,4 +1,19 @@
 module.exports = function(doc) {
-  console.log("transformer: " + JSON.stringify(doc))
-  return doc
+
+    /**
+     * Add data filter, only store the related fields into elatissearch.
+     */
+    
+    if (doc.ns == "opentown.users") {
+        doc["data"] = _.pick(doc.data, ["_id", "nickname", "deleted", "phone", "intro"]);
+    } else
+    if (doc.ns == "opentown.topics") {
+        doc["data"] = _.pick(doc.data, ["_id", "title", "abstract", "deleted", "statements"]);
+        if (doc.data && doc.data.statements) {
+            doc.data.statements = _.pick(doc.data.statements, ["content"]);
+        }
+    }
+
+    console.log("transformer: " + JSON.stringify(doc))
+    return doc
 }
